4bc1ca27b0e3d9e630cd32bccd654a999d6e5f62
James King <jk@science-practice.com>
2023-03-06 13:28:48 +0000

    Complete exercise 5

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,9 +3,10 @@ import React from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput";
-import GuessResults from "../GuessResults"
-import {NUM_OF_GUESSES_ALLOWED} from "../../constants"
-import {checkGuess} from "../../game-helpers"
+import GuessResults from "../GuessResults";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { checkGuess } from "../../game-helpers";
+import Result from "../Result";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -15,23 +16,33 @@ console.info({ answer });
 function Game() {
   const [guess, setGuess] = React.useState("");
   const [results, setResults] = React.useState([]);
+  const [gameStatus, setGameStatus] = React.useState("playing");
 
   function handleGuess() {
-    if(results.length >= NUM_OF_GUESSES_ALLOWED){
-      window.alert('Max number of guesses reached!')
-      return
+    const result = checkGuess(guess, answer);
+    const resultCorrect = result.every((entry) => entry.status === "correct");
+    if (resultCorrect) {
+      setGameStatus("win");
+    } else if(results.length + 1 >= NUM_OF_GUESSES_ALLOWED){
+      setGameStatus("lose")
     }
-    const result = checkGuess(guess, answer)
-    console.log(`Guess: ${guess}`);
+    console.log({ resultCorrect });
     const nextResults = [...results, result];
     setResults(nextResults);
-    console.log({nextResults});
     setGuess("");
   }
   return (
     <>
-      <GuessResults results={results} maxGuesses={NUM_OF_GUESSES_ALLOWED}/>
-      <GuessInput guess={guess} setGuess={setGuess} handleGuess={handleGuess}/>
+      <GuessResults results={results} maxGuesses={NUM_OF_GUESSES_ALLOWED} />
+      {gameStatus != "playing" ? (
+        <Result gameStatus={gameStatus} numGuesses={results.length} answer={answer} />
+      ) : (
+        <GuessInput
+          guess={guess}
+          setGuess={setGuess}
+          handleGuess={handleGuess}
+        />
+      )}
     </>
   );
 }
